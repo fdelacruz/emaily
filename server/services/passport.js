71c547ba386a2a30f80ed3e99f77bc7df297a046
Francisco De La Cruz <helloquico@gmail.com>
2017-08-08 22:17:06 -0400

    Mongoose Queries

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,7 +13,15 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+          if (existingUser) {
+            // We already have a record with the given profile ID
+            // Do Nothing
+          } else {
+            // We don't have an user record with this ID, make a new one
+            new User({ googleId: profile.id }).save();
+          }
+        })
     }
   )
 );
