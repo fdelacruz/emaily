7a2213e23128cbfc27ff80a4bb56bcb125297a53
Francisco De La Cruz <helloquico@gmail.com>
2017-08-16 22:36:36 -0400

    Header Content

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,11 +5,11 @@ class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return 'Still deciding';
+        return;
       case false:
-        return 'Im loggedout';
+        return <li><a href="/auth/google">Login with Google</a></li>;
       default:
-        return 'Im logged in';
+        return <li><a>Logout</a></li>;
     }
   }
 
