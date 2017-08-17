852f8b6af8e440a997ea743c0fe3eed2cd336fba
Francisco De La Cruz <helloquico@gmail.com>
2017-08-16 22:28:27 -0400

    Access State in the Header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Still deciding';
+      case false:
+        return 'Im loggedout';
+      default:
+        return 'Im logged in';
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -9,9 +21,7 @@ class Header extends Component {
             Emaily
           </a>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,4 +29,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
