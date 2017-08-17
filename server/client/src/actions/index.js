77557ef39b70b7726dffa5f4e1a764c11e46c845
Francisco De La Cruz <helloquico@gmail.com>
2017-08-16 21:41:31 -0400

    AuthReducer Return Values

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -4,5 +4,5 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => dispatch => {
     axios
       .get('/api/current_user')
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+      .then(res => dispatch({ type: FETCH_USER, payload: res.data }));
   };
