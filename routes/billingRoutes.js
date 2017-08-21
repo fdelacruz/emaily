90aaf54b38034f84fd9597e4e94b4866c8ca107b
Francisco De La Cruz <helloquico@gmail.com>
2017-08-21 17:48:18 -0400

    Require Authentication

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,10 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: 'You must log in!' });
+    }
+
     stripe.charges.create({
       amount: 500,
       currency: 'usd',
